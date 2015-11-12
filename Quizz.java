e87472df6b6c68d64b307bd801f69e4206679ba1
pedronunovilhena <pedronunovilhena@gmail.com>
2015-11-12 18:46:22 +0000

    Ultima versão funcional com quizz (apenas UI)

--- Quizz.java
+++ Quizz.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Quizz extends Activity {
+    public int answer_number;
+    public int player_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,54 @@ public class Quizz extends Activity {
 
     public void Start_Questions (){
 
+            // fix, o fogg e o passepartout
+            //set the question text from current question
+            String question = "Qual das seguintes personagens gosta mais ?";
+            TextView qText = (TextView) findViewById(R.id.question);
+            qText.setText(question);
+
+            TextView option1 = (TextView) findViewById(R.id.answer1);
+            option1.setText("Fix");
+
+            TextView option2 = (TextView) findViewById(R.id.answer2);
+            option2.setText("Fogg");
+
+            TextView option3 = (TextView) findViewById(R.id.answer3);
+            option3.setText("Passepartout");
+    }
+
+    public void getSelectedAnswer(View v){
+        String answer=getTextAnswer();
+        Toast.makeText(getApplicationContext(), answer, Toast.LENGTH_SHORT).show();
+        answer_number++;
+        if(answer_number==3){
+            showQuizzResults();
+        }
+    }
+
+    public void showQuizzResults(){
+
+
+    }
+
+    private String getTextAnswer() {
+        RadioButton c1 = (RadioButton)findViewById(R.id.answer1);
+        RadioButton c2 = (RadioButton)findViewById(R.id.answer2);
+        RadioButton c3 = (RadioButton)findViewById(R.id.answer3);
+        if (c1.isChecked())
+        {
+            return c1.getText().toString();
+        }
+        if (c2.isChecked())
+        {
+            return c2.getText().toString();
+        }
+        if (c3.isChecked())
+        {
+            return c3.getText().toString();
+        }
+
+        return null;
     }
 
     @Override
